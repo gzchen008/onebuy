f1ca03591a4e4e0f430e15317c8e9f2d19f367cd
cgz <vsjosonadmin@163.com>
2015-10-09 21:20:33 +0800

    new

--- src/test/java/com/xianchumo/shop/test/entity/CartItemTest.java
+++ src/test/java/com/xianchumo/shop/test/entity/CartItemTest.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.xianchumo.shop.test.entity;
 
 import java.util.HashSet;
@@ -30,3 +31,37 @@ public class CartItemTest {
 		}
 	}
 }
+=======
+package com.xianchumo.shop.test.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+import com.xianchumo.shop.entity.CartItem;
+import com.xianchumo.shop.entity.Good;
+
+public class CartItemTest {
+	@Test
+	public void test() {
+		Set<CartItem> set = new HashSet<CartItem>();
+		CartItem e = new CartItem();
+		CartItem e2 = new CartItem();
+		Good good = new Good();
+		Good good2 = new Good();
+		good.setGid(1l);
+		good2.setGid(1l);
+		good.setName("abc");
+		good2.setName("23222");
+		e.setGood(good);
+		e2.setGood(good2);
+		set.add(e);
+		set.add(e2);
+		System.out.println(set.size());
+		for (CartItem c : set) {
+			System.out.println(c.getGood().getName());
+		}
+	}
+}
+>>>>>>> 35f6b20b617e27eb9f1710f08402aade8225cd9d
--- src/test/java/com/xianchumo/shop/test/service/TestTemp.java
+++ src/test/java/com/xianchumo/shop/test/service/TestTemp.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.xianchumo.shop.test.service;
 
 import org.junit.Test;
@@ -23,3 +24,30 @@ public class TestTemp {
 			System.out.println(ShopUtil.createOrderNumber());
 	}
 }
+=======
+package com.xianchumo.shop.test.service;
+
+import org.junit.Test;
+
+import com.xianchumo.shop.util.ShopUtil;
+
+public class TestTemp {
+	@Test
+	public void test() {
+		System.out.println('Z' + 0);
+		System.out.println('A' + 0);
+
+		for (int i = 0; i < 200; i++) {
+			long t = System.currentTimeMillis();
+			int a = (int) (Math.random() * 100);
+			System.out.println((int) (t + a) / 20);
+		}
+
+	}
+
+	@Test
+	public void testUtil() {
+			System.out.println(ShopUtil.createOrderNumber());
+	}
+}
+>>>>>>> 35f6b20b617e27eb9f1710f08402aade8225cd9d
